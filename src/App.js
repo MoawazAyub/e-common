fb625c7efecf58e68c0056714c4b0e5e3b1f34af
Moawaz Ayub <moawazayubmmm@gmail.com>
2021-02-06 19:56:10 +0500

    added homepage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomePage from './components/homepage/homepage';
 
 function App() {
   return (
     <div>
-     Hello Moawaz
+      <HomePage />
     </div>
   );
 }
