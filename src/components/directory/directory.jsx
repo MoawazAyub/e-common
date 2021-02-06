bbc64bc7d90f2bc9c5899b590243e253cda3fbab
Moawaz Ayub <moawazayubmmm@gmail.com>
2021-02-06 23:05:03 +0500

    refactored directory structure

---
+++ src/components/directory/directory.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import MenuItem from '../menu-item/menu-item';
+
+import './directory.scss';
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: 'Luxury Apartment',
+          imageUrl: 'https://thisrentaldoesnotexist.com/img-new/img4.jpg',
+          id: 1
+        },
+        {
+          title: 'Condo Apartment',
+          imageUrl: 'https://thisrentaldoesnotexist.com/img-new/img2.jpg',
+          id: 2
+        },
+        {
+          title: 'Business Apartments',
+          imageUrl: 'https://thisrentaldoesnotexist.com/img-new/img1.jpg',
+          id: 3
+        },
+        {
+          title: 'Town House',
+          imageUrl: 'https://thisrentaldoesnotexist.com/img-new/hero.jpg',
+          size: 'large',
+          id: 4
+        },
+        {
+          title: 'Mentions',
+          imageUrl: 'https://thisrentaldoesnotexist.com/img-new/img3.jpg',
+          size: 'large',
+          id: 5
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className='directory-menu'>
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
