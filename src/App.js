9896eaebc9e1f4e48e010d984aaeb588e278a931
Moawaz Ayub <moawazayubmmm@gmail.com>
2021-03-18 16:11:03 +0500

    added sign in component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header.component.jsx';
+import ShopPageSignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route path='/shop' component={ShopPage}></Route>
+        <Route path='/signin' component={ShopPageSignInAndSignUpPage}></Route>
       </Switch>
     </div>
   );
